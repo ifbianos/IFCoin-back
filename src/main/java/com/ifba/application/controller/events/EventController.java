aedeff727a4e558297fe1a7f90b718aee4bcf1ec
Rafael Campos Mendonça <rafael.campos.mendonca@hotmail.com>
2022-06-04 18:37:51 -0300

    nem lembro

--- src/main/java/com/ifba/application/controller/events/EventController.java
+++ src/main/java/com/ifba/application/controller/events/EventController.java
@@ -2,18 +2,14 @@ package com.ifba.application.controller.events;
 
 
 import com.ifba.config.modelmapper.eventconvert.EventConvert;
-import com.ifba.domain.dto.event.EventDTO;
 import com.ifba.domain.dto.event.EventRequestDto;
-import com.ifba.domain.entity.Student;
 import com.ifba.domain.usecase.event.EventUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
